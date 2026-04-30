bffcbf60ab73fe47b7ff9a629e1966e9f2509619
MusoZorin <muso20132013@gmail.com>
2026-04-30 11:02:45 +0500

    Update comments in lineage_merlinx.mk

--- lineage_merlinx.mk
+++ lineage_merlinx.mk
@@ -1,17 +1,14 @@
-#
 # Copyright (C) 2022 The LineageOS Project
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
-# Inherit from those products. Most specific first.
+# Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from device makefile.
+# Inherit from device makefile
 $(call inherit-product, device/xiaomi/merlinx/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_merlinx
